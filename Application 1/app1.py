14657933b99bc72fd34e87ad5d82d5607b6a1c6c
Mikhail Hyde <hyde.mikhail@gmail.com>
2019-09-15 22:37:04 -0400

    First Commit

---
+++ Application 1/app1.py
@@ -0,0 +1,21 @@
+import json
+
+def loadData(file):
+    return json.load(open("E:\\Documents\\Scripts\\Python Mega Course\\Application 1\\%s" % file))
+
+def getDefinition(dictionary, word):
+    return dictionary[word]
+
+def main():
+    dictionary = loadData("data.json")
+    while True:
+        usrInput = str(input("Enter word: ")).lower()
+        if usrInput == "\\end":
+            break
+        elif usrInput in dictionary:
+            print(getDefinition(dictionary, usrInput))
+        else:
+            print("Word not recognized. Try again.")
+    print("Thanks for using the dictionary!")
+
+main()
