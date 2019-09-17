4cc2a5b161844f15e41be4b02c99ab6bb12d7c61
Mikhail Hyde <mhyde64@my.uri.edu>
2019-09-17 16:28:01 -0400

    Loaded 3 DataFrames: CSV, XLSX, and JSON

--- Application 2/pandasPrac.py
+++ Application 2/pandasPrac.py
@@ -1,5 +1,16 @@
+import os
 import pandas
 
+'''
 dataFrame = pandas.DataFrame([[2, 4, 6], [10, 20, 30]], columns = ["Col 1", "Col 2", "Col 3"], index = ["First", "Second"])
-
 print(dataFrame)
+'''
+
+dataFrame1 = pandas.read_csv("U:\\Documents\\GitCode\\UdemyPython\\Application 2\\PandasCSV\\supermarkets.csv")
+dataFrame2 = pandas.read_excel("U:\\Documents\\GitCode\\UdemyPython\\Application 2\\PandasCSV\\supermarkets.xlsx")
+dataFrame3 = pandas.read_json("U:\\Documents\\GitCode\\UdemyPython\\Application 2\\PandasCSV\\supermarkets.json")
+print(dataFrame1)
+print("\n\n")
+print(dataFrame2)
+print("\n\n")
+print(dataFrame3)
