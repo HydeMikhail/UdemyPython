b5d590ac617c9ade4e70ab50a39300817b35c491
Mikhail Hyde <hyde.mikhail@gmail.com>
2019-09-16 21:52:00 -0400

    Nearly finished application 1

--- Application 1/app1.py
+++ Application 1/app1.py
@@ -1,8 +1,5 @@
 import json
-
-'''
-A test to work with VIM editor
-'''
+from difflib import get_close_matches
 
 def loadData(file):
     #Home File Location: "E:\\Documents\\Scripts\\Udemy Classes\\UdemyPython\\Application 1\\%s" % file
@@ -13,15 +10,32 @@ def getDefinition(dictionary, word):
     return dictionary[word]
 
 def main():
+    
+    print("\n\nWelcome to our dictionary! \nType in a word when prompted and the definition will be displayed on the screen!\n\n================================\nTo exit the program, enter \\end\n================================")
     dictionary = loadData("data.json")
+
     while True:
+
+        print("\n")
         usrInput = str(input("Enter word: ")).lower()
+
         if usrInput == "\\end":
             break
+
         elif usrInput in dictionary:
             print(getDefinition(dictionary, usrInput))
-        else:
-            print("Word not recognized. Try again.")
-    print("Thanks for using the dictionary!")
+
+        elif len(get_close_matches(usrInput, dictionary.keys())) > 0:
+
+            print("Did you mean %s instead?" % get_close_matches(usrInput, dictionary.keys())[0])
+            usrChoice = input("Enter yes [y] or no [n]: ").lower()
+
+            if usrChoice == "y":
+                print(getDefinition(dictionary, get_close_matches(usrInput, dictionary.keys())[0]))
+
+            else:
+                print("Entry no recognized. Please try again.")
+
+    print("\nThanks for using the dictionary!")
 
 main()
