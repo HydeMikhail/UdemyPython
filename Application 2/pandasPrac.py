7adf963739707374d0cf9ed98e05a3788f98fcd7
Mikhail Hyde <mhyde64@my.uri.edu>
2019-09-23 13:18:41 -0400

    What

--- Application 2/pandasPrac.py
+++ Application 2/pandasPrac.py
@@ -9,11 +9,11 @@ print(dataFrame)
 #Home File Location: "E:\\Documents\\Scripts\\Udemy Classes\\UdemyPython\\Application 2\\PandasCSV\\XXXXX"
 #Work File Location: "U:\\Documents\\GitCode\\UdemyPython\\Application 2\\PandasCSV\\XXXXX"
 
-dataFrame1 = pandas.read_csv("E:\\Documents\\Scripts\\Udemy Classes\\UdemyPython\\Application 2\\PandasCSV\\supermarkets.csv")
-dataFrame2 = pandas.read_excel("E:\\Documents\\Scripts\\Udemy Classes\\UdemyPython\\Application 2\\PandasCSV\\supermarkets.xlsx", sheet_name = 0)
-dataFrame3 = pandas.read_json("E:\\Documents\\Scripts\\Udemy Classes\\UdemyPython\\Application 2\\PandasCSV\\supermarkets.json")
-dataFrame4 = pandas.read_csv("E:\\Documents\\Scripts\\Udemy Classes\\UdemyPython\\Application 2\\PandasCSV\\supermarkets-commas.txt")
-dataFrame5 = pandas.read_csv("E:\\Documents\\Scripts\\Udemy Classes\\UdemyPython\\Application 2\\PandasCSV\\supermarkets-semi-colons.txt", sep = ";")
+dataFrame1 = pandas.read_csv("U:\\Documents\\GitCode\\UdemyPython\\Application 2\\PandasCSV\\supermarkets.csv")
+dataFrame2 = pandas.read_excel("U:\\Documents\\GitCode\\UdemyPython\\Application 2\\PandasCSV\\supermarkets.xlsx", sheet_name = 0)
+dataFrame3 = pandas.read_json("U:\\Documents\\GitCode\\UdemyPython\\Application 2\\PandasCSV\\supermarkets.json")
+dataFrame4 = pandas.read_csv("U:\\Documents\\GitCode\\UdemyPython\\Application 2\\PandasCSV\\supermarkets-commas.txt")
+dataFrame5 = pandas.read_csv("U:\\Documents\\GitCode\\UdemyPython\\Application 2\\PandasCSV\\supermarkets-semi-colons.txt", sep = ";")
 
 print('''
 
